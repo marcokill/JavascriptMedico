1d5fd190ff3446ce95e2cc0bf65e335178494046
Escobar Ruiz Marco Antonio <151296298+marcokill@users.noreply.github.com>
2024-06-02 03:16:10 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-
-const PORT = process.env.PORT || 8000;
-const serverMessage = `Servidor en ejecución en el puerto ${PORT}`;
-
-app.listen(PORT, () => {
-  console.log(serverMessage);
-});
